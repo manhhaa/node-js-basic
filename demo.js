09ed119b47849af0de5bfa9750558d32e9f6797b
manhhaa <hanguyenns793@gmail.com>
2022-11-16 16:39:09 +0700

    a

--- demo.js
+++ demo.js
@@ -1,19 +1,12 @@
-let http = require("http");
-let url = require("url");
-let fs = require("fs");
+const express = require("express");
 
-http
-  .createServer(function (req, res) {
-    let q = url.parse(req.url, true);
-    let filename = "." + q.pathname;
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("404 Not Found");
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(8080);
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
